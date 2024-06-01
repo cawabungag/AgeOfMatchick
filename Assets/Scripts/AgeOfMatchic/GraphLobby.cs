088a6b287d3d840384e07d390de597a99ce8465d
Ilyas Ilyasov <38839317+cawabungag@users.noreply.github.com>
2024-06-01 20:22:11 +0300

    add lobby

---
+++ Assets/Scripts/AgeOfMatchic/GraphLobby.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class GraphLobby : MonoBehaviour
+{
+	public Node StartNode;
+}
---
+++ Assets/Scripts/AgeOfMatchic/Node.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+   public Vector2 Coord;
+   public Node[] Next;
+   public Node[] Preview;
+}
